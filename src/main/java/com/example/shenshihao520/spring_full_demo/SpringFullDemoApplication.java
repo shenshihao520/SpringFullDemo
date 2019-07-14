143047398d3cceb2d157f4c0287835f56de7e944
794262212@qq.com <794262212@qq.com>
2019-07-14 15:24:01 +0800

    initProject

---
+++ src/main/java/com/example/shenshihao520/spring_full_demo/SpringFullDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.shenshihao520.spring_full_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFullDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFullDemoApplication.class, args);
+	}
+
+}
